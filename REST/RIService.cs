f8bb7e52727eb387814afbc6fa429fa85251e386
Hebatallah <Hebatallah@DESKTOP-J732E08>
2017-03-08 19:47:26 +0200

    REST Service

---
+++ REST/RIService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI;
+
+namespace REST
+{
+
+    [ServiceContract]
+    public interface RIService
+    {
+        // http://.........../Employees
+        [WebGet(UriTemplate = "Employees")]
+        [OperationContract]
+        DataSet GetAll();
+
+        // http://.........../Employee/1
+        [WebGet(UriTemplate = "Employee/{EMP_ID}")]
+        [OperationContract]
+        DataSet GetByID(string EMP_ID);
+
+
+        // POST http://.........../Employee
+        [WebInvoke(Method = "POST", UriTemplate = "Employee")]
+        [OperationContract]
+        int Insert(string EMP_ID, string Name, string DP_ID);
+
+        // PUT http://.........../Employee/1
+        [WebInvoke(Method = "PUT", UriTemplate = "Employee/{EMP_ID}")]
+        [OperationContract]
+        int Update(string EMP_ID, string Name, string DP_ID);
+
+
+        [WebInvoke(Method = "DELETE", UriTemplate = "Employee/{EMP_ID}")]
+        [OperationContract]
+        int Delete(string EMP_ID);
+
+
+
+
+
+
+
+    }
+}
---
+++ REST/RService.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REST
+{
+    class RService : RIService
+    {
+        SqlConnection con = new SqlConnection("Data Source =.\\MSSQL; Initial Catalog = WCFProject; Integrated Security = True");
+        SqlCommand com;
+        DataSet ds;
+        SqlDataAdapter adapter;
+        public DataSet GetAll()
+        {
+            com = new SqlCommand();
+            com.Connection = con;
+            com.CommandType = CommandType.Text;
+            com.CommandText = "select a.* from Emp a,Departments b where a.DP_ID=b.DP_ID";
+            ds = new DataSet();
+            adapter = new SqlDataAdapter(com);
+            adapter.Fill(ds);
+            return ds;
+        }
+        public DataSet GetByID(string EMP_ID)
+        {
+            com = new SqlCommand();
+            com.Connection = con;
+            com.CommandType = CommandType.Text;
+            com.CommandText = "select a.*,b.DP_Name from Emp a,Departments b where a.DP_ID=b.DP_ID and a.EMP_ID=" + int.Parse(EMP_ID);
+            ds = new DataSet();
+            adapter = new SqlDataAdapter(com);
+            adapter.Fill(ds);
+            return ds;
+        }
+        public int Insert(string EMP_ID, string Name,string DP_ID)
+        {
+            com = new SqlCommand();
+            com.Connection = con;
+            com.CommandType = CommandType.Text;
+            com.CommandText = "insert into Emp values (" + int.Parse(EMP_ID )+ ",'" + Name + "'," +int.Parse( DP_ID) + ")";
+            con.Open();
+            int affected = com.ExecuteNonQuery();
+            con.Close();
+            return affected;
+
+        }
+
+        public int Update(string EMP_ID, string Name,string DP_ID)
+        {
+            com = new SqlCommand();
+            com.Connection = con;
+            com.CommandType = CommandType.Text;
+            com.CommandText = "update Emp set EMP_Name='" + Name + "',DP_ID=" + int.Parse(DP_ID )+ "where EMP_ID=" +int.Parse(EMP_ID);
+            con.Open();
+            int affected = com.ExecuteNonQuery();
+            con.Close();
+            return affected;
+
+        }
+
+        public int Delete(string EMP_ID)
+        {
+            com = new SqlCommand();
+            com.Connection = con;
+            com.CommandType = CommandType.Text;
+            com.CommandText = "Delete from Emp where EMP_ID=" +int.Parse(EMP_ID);
+            con.Open();
+            int affected = com.ExecuteNonQuery();
+            con.Close();
+            return affected;
+
+        }
+
+
+
+
+    }
+
+}
